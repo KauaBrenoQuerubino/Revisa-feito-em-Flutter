e8a36a619e39f4d11c40b869acee994d326cba3e
Kaua Breno <vaibetan009@gmail.com>
2026-06-16 23:49:03 -0300

    Index criado

--- lib/Compents/App_widget.dart
+++ lib/Compents/App_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:revisai/Compents/Page/auth/Cadastro_page.dart';
 import 'package:revisai/Compents/Page/auth/Login_page.dart';
+import 'package:revisai/Compents/Page/index/Index_page.dart';
 
 class AppWidget extends StatelessWidget{
 
@@ -12,8 +13,9 @@ class AppWidget extends StatelessWidget{
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
-      initialRoute: '/login',
+      initialRoute: '/',
       routes: {
+        '/' : (context) => IndexPage(),
         '/login' : (context) => LoginPage(),
         '/cadastra-se' : (context) => CadastroPage(),
       } ,
--- lib/Compents/Page/index/Index_page.dart
+++ lib/Compents/Page/index/Index_page.dart
@@ -8,8 +8,66 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+
+
+  Widget _NavBar(){
+     return Container(
+      width: double.infinity,
+      height: 250.0,
+
+      child: Stack(
+
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(40),
+            ),
+            child: Image.asset(
+              "assets/images/background_primary.png",
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+           
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Text("Logo aqui"),
+                  Text("Usuario icon aqui")
+                ],
+              ),
+              Column(
+                children:[  Text("Ola, usuarioName"), Text("Bem vindo de volta")],
+              ),
+              Container(
+                height: 50,
+              ),
+            ],
+          ),
+        ]
+      )
+    );
+  }
+
+  Widget _body() {
+   return SizedBox();
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: Stack(
+        children: [
+          _NavBar(),
+          _body()
+        ],
+
+      ),
+    );
   }
 }
